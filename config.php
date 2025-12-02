8cfec4657e9204563f706a86537a9ae5c6303ca9
VietAnh <22070975@vnu.edu.vn>
2025-12-02 17:58:45 +0700

    Change database connection settings and error handling
    Updated database connection parameters and error message.

--- config.php
+++ config.php
@@ -1,13 +1,18 @@
 <?php
+$servername = "sql100.infinityfree.com";
+$username = "if0_40575788";
+$password = "nhom12345678";
+$dbname = "if0_40575788_destinations";
+$port = 3306;
 
+// Kết nối MySQL
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "travel_db";
+// Set UTF8
+$conn->set_charset("utf8mb4");
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Kiểm tra lỗi
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Kết nối thất bại: " . $conn->connect_error);
 }
 ?>
