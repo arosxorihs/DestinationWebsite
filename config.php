4e470c37b8fb24155fe3c22fcda4d99823db280a
buiminhduc <bmd.a2vth@gmail.com>
2025-12-08 17:48:21 +0700

    change
    thay đổi lại như cũ

--- config.php
+++ config.php
@@ -1,23 +1,17 @@
 <?php
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$dbname     = "travel_db";  // ⬅ đúng tên database bạn tạo
 
-if (session_status() === PHP_SESSION_NONE) {
-  session_start();
-}
+// Tạo kết nối
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// THONG TIN KET NOI 
-$host = 'sql100.infinityfree.com'; // MYSQL HOSTNAME
-$user = 'ifo_40506311';           // MYSQL USERNAME
-$pass = 'buiduc2312';             // MYSQL PASSWORD
-$db   = 'if0_40506311_test';       // MYSQL DATABASE NAME 
-
-$conn = new mysqli($host, $user, $pass, $db);
+// Kiểm tra kết nối
 if ($conn->connect_error) {
-  die('Connection failed: ' . $conn->connect_error);
-}
-$conn->set_charset('utf8mb4');
-
-function current_user_id() {
-  return isset($_SESSION['user_id']) ? (int)$_SESSION['user_id'] : null;
+    die("Connection failed: " . $conn->connect_error);
 }
 
+// Set charset để tránh lỗi tiếng Việt
+$conn->set_charset("utf8mb4");
 ?>
