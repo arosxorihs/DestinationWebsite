c6d80362252f4d0a73d90de143079ed8d875cd32
buiminhduc <bmd.a2vth@gmail.com>
2025-12-08 16:10:52 +0700

    thử trên domain mới
    thay thế kết nối domain cũ và thử trên domaiin mới

--- config.php
+++ config.php
@@ -1,17 +1,23 @@
 <?php
-$servername = "localhost";
-$username   = "root";
-$password   = "";
-$dbname     = "travel_db";  // ⬅ đúng tên database bạn tạo
 
-// Tạo kết nối
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Kiểm tra kết nối
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (session_status() === PHP_SESSION_NONE) {
+  session_start();
 }
 
-// Set charset để tránh lỗi tiếng Việt
-$conn->set_charset("utf8mb4");
+// THONG TIN KET NOI 
+$host = 'sql100.infinityfree.com'; // MYSQL HOSTNAME
+$user = 'ifo_40506311';           // MYSQL USERNAME
+$pass = 'buiduc2312';             // MYSQL PASSWORD
+$db   = 'if0_40506311_test';       // MYSQL DATABASE NAME 
+
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) {
+  die('Connection failed: ' . $conn->connect_error);
+}
+$conn->set_charset('utf8mb4');
+
+function current_user_id() {
+  return isset($_SESSION['user_id']) ? (int)$_SESSION['user_id'] : null;
+}
+
 ?>
