a3da7091769f1eacd3870544a899a3ac7492af5b
ThaiTran04 <thai06092004@gmail.com>
2026-01-16 12:39:05 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,17 @@
 <?php
-$servername = "localhost";
-$username   = "root";
-$password   = "";
-$dbname     = "travel_db";
+$servername = "sql100.infinityfree.com";
+$username = "if0_40575788";
+$password = "nhom12345678";
+$dbname = "if0_40575788_destinations";
+$port = 3306;
+
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 $conn->set_charset("utf8mb4");
 ?>
